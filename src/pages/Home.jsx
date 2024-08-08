4e196773d493641304fc7c5b52d8216157b50cb8
braitcode <brightjohn489@gmail.com>
2024-08-08 14:44:25 +0100

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             tech studio is cool
         </p>
         <button className='btn btn_primary'>bright</button>
-        <button className='btn btn_primary'> peace</button>
+        <p className='btn btn_primary'> peace</p>
     </div>
   )
 }
