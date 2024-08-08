188dab62baebb552e76e3f1f8d5a186b7bd177a8
Oluwabukola-G <amoooluwabukola@outlook.com>
2024-08-08 15:12:26 +0100

    fix

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <p className='tech'>
+            tech studio is cool
+        </p>
+        <button className='btn btn_primary'>bright</button>
+        <p className='btn btn_primary'> peace</p>
+    </div>
+  )
+}
+
+export default Home
