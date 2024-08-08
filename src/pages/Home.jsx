b0334041ae1538419e0a11add4fd950934419502
braitcode <brightjohn489@gmail.com>
2024-08-08 14:29:46 +0100

    new commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <p className='tech'>
+            tech studio is cool
+        </p>
+        <button className='btn btn_primary'>bright</button>
+    </div>
+  )
+}
+
+export default Home
