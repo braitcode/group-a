b6ad3485a436713e12ff2c3f9ea3739c2cde6588
manifesto <manifestomixx@gmail.com>
2024-08-08 14:55:43 +0100

    update:testing for conflict

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p className='tech'>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h3 className='tech'>Edit and save to test HMR settings using something new and save to test HMR settings using something new</h3>
         <p className='btn btn_primary'>new button</p>
       </div>
       <p className="read-the-docs">
@@ -32,7 +30,7 @@ function App() {
       </p>
       <div>
         
-        <h3 className='tech'>Edit and save to test HMR settings using something new and save to test HMR settings using something new</h3>
+       
       </div>
       
     </>
